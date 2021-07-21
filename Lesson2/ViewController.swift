6bcc7018a333d6cc8483c6bc0fe70a91bd15f936
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-07-21 21:06:36 +0300

    switch

--- Lesson2/ViewController.swift
+++ Lesson2/ViewController.swift
@@ -9,11 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+   
+    var variable: Int = 123
+    
+    var string: String = "asd\nйцу"  //перевод на новую строку
+   
+var stringVariable: String = """
+    qwe
+    sda
+    fvxz
+    erg
+    """
+    //все значения с новой строки
+    
+    var doubleVariable = 1.234
+    
+    let z: Int8 = 127 // от -128 до 128 = 256
+    
+    let z1: Int8 = 125
+    
+    var charVariable: Character = "a"
+    
+    var boolVariable: Bool = true
+    
+   
+  
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    print (string)
+        print (stringVariable)
+        
+        let equalVariable: Bool =  z != z1 //сравнение
+        
+        print (equalVariable, z1) //вывод нескольких переменных через пробел
+    
+      
+    
+        
+        let string1: String = "123"
+        let int1: Int = Int(string1)!    //строку переделали в
+        print (int1)
+        
+        
+        let double: Double = 123.213234231324232
+        let float: Float = Float(double)       //из double перевели во float
+        
+        print (float)
+        
+        
+        let int2: Int = 123
+        let float2: Float = 123
+        let double2: Double = Double(int2) / Double(float2)
+        print(double2)
+    
+    
+        switch "1234"{
+        case "1234": print (1234)
+        case "123": print (123)
+        default: print ("нет значения")
+        }
+    
+    
+    
+    
+    
+    
+    
+    
     }
 
 
 }
+
 
